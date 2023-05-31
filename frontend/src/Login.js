0b307a11c7becd90e8c07716767f32d31d27d595
ramesh06190 <apvs.ramesh@gmail.com>
2023-05-30 19:34:52 -0500

    Added Login and Signup Validations

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,31 +1,36 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
+import validation from "./LoginValidation";
 function Login() {
     const [values, setValues] = useState({
         email: '',
         password: ''
     })
+    const [errors, setErrors] = useState
     const handleInput =(event) => {
         setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
     }
     const handleSubmit =(event) => {
         event.preventDefault();
+        setErrors(validation(values));
     }
     return (
         <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
-                <form action="">
+                <form action="" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="email"><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' 
+                    <input type="email" placeholder='Enter Email' name='email'
                     onChange={handleInput} className='form-control'/>
+                    {errors.email && <span className="text-danger"> {errors.email} </span>}
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password"><strong>Password</strong></label>
-                    <input type="password" placeholder='Enter Password' 
+                    <input type="password" placeholder='Enter Password' name='password'
                     onChange={handleInput} className='form-control'/>
+                    {errors.password && <span className="text-danger"> {errors.password} </span>}
                 </div>
-                <button className="btn btn-success w-100">Log in</button>
+                <button type='submit' className="btn btn-success w-100">Log in</button>
                 <p>You are agree to our terms and policies</p>
                 <Link to="/signup" className="btn btn-default border w-100 bg-light">Create Account</Link>
                 </form>
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import validation from "./SignupValidation";
+
 function Signup() {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+    const [errors, setErrors] = useState
+    const handleInput =(event) => {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
+    const handleSubmit =(event) => {
+        event.preventDefault();
+        setErrors(validation(values));
+    }
+
     return(
         <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
-                <form action="">
+                <form action="" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name"><strong>Name</strong></label>
-                    <input type="text" placeholder='Enter Name' className='form-control'/>
+                    <input type="text" placeholder='Enter Name' name='name'
+                    onChange={handleInput} className='form-control'/>
+                    {errors.name && <span className="text-danger"> {errors.name} </span>}
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email"><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' className='form-control'/>
+                    <input type="email" placeholder='Enter Email' name='email'
+                    onChange={handleInput} className='form-control'/>
+                    {errors.email && <span className="text-danger"> {errors.email} </span>}
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password"><strong>Password</strong></label>
-                    <input type="password" placeholder='Enter Password' className='form-control'/>
+                    <input type="password" placeholder='Enter Password' name='password'
+                    onChange={handleInput} className='form-control'/>
+                    {errors.password && <span className="text-danger"> {errors.password} </span>}
                 </div>
-                <button className="btn btn-success w-100">Sign up</button>
+                <button type='submit' className="btn btn-success w-100">Sign up</button>
                 <p>You are agree to our terms and policies</p>
                 <Link to="/" className="btn btn-default border w-100 bg-light">Login</Link>
                 </form>
