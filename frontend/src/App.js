d5f177da26411f5433064fb02d13f417ec550de5
ramesh06190 <apvs.ramesh@gmail.com>
2023-05-31 23:25:02 -0500

    Added Welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,17 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Signup from './Signup';
 import Home from './Home';
 
+
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Login />}></Route>
+      <Route path='/' element={<Welcome />}></Route>
+      <Route path='/login' element={<Login />}></Route>
       <Route path='/signup' element={<Signup />}></Route>
       <Route path='/home' element={<Home />}></Route>
+   
+
     </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Login.css';
+
+function Welcome() {
+    return (
+        <div className= 'welcome-container' o>
+            <Link to="/" className="btn btn-primary border w-100 ">Welcome</Link>
+        </div>
+    )
+}
+
+export default Welcome
