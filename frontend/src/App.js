6a8481f77bd7e7fc1eae1fdacdb4c9b3180c7093
ramesh06190 <apvs.ramesh@gmail.com>
2023-09-10 18:19:41 -0500

    Added Menu Search Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import Login  from './Login';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Signup from './Signup';
 import Home from './Home';
+import Login from './Login';
 
+import Menu from './Menu';
+import Reservation from './Reservation';
+import AboutUs from './AboutUs';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <div>
+    
     <Routes>
-      <Route path='/' element={<Login />}></Route>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/menu' element={<Menu />}></Route>
+      <Route path='/reservation' element={<Reservation />}></Route>
+      <Route path='/about-us' element={<AboutUs />}></Route>
       <Route path='/signup' element={<Signup />}></Route>
-      <Route path='/home' element={<Home />}></Route>
+      <Route path='/login' element={<Login />}></Route>
    
 
     </Routes>
+    </div>
     </BrowserRouter>
   )
 }
