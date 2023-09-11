6523531382ce4ed36a88de043cc40dbb4e91abe3
JAGADEESH PONNAM <112121032+Jagadeeshponnam@users.noreply.github.com>
2023-09-10 21:35:56 -0500

    Added AboutUs.js

---
+++ frontend/src/AboutUs.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Header from './Header';
+
+ 
+
+function AboutUs() {
+
+  return (
+
+    <div>
+
+        <Header />
+
+      <h1>Still Under Progress</h1>
+
+      {/* Add menu content here */}
+
+    </div>
+
+  );
+
+}
+
+ 
+
+export default AboutUs;
