8b658d031004232b87deb9048843e8fd307f2c30
ramesh06190 <apvs.ramesh@gmail.com>
2023-11-03 14:38:58 -0500

    Updated Maps and Track

---
+++ frontend/src/Components/Maps.js
@@ -0,0 +1,157 @@
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from '@react-google-maps/api'
+
+const center = { lat: 8.307736, lng: 77.220462 }
+
+function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyCIWbUh6hK1P_ARYXLVwqm2B_IOeACS8is",
+    libraries: ['places'],
+  })
+
+  const latitude = parseFloat(localStorage.getItem("latitude"));
+  const longitude = parseFloat(localStorage.getItem("longitude"));
+
+  const concatenatedValue = `${latitude.toFixed(6)}, ${longitude.toFixed(6)}`;
+  console.log(concatenatedValue);
+  useEffect(()=>{
+    calculateRoute()
+    
+  },[])
+
+ 
+ 
+  const [map, setMap] = useState(null)
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
+  const [origin, setOrigin] = useState(concatenatedValue)
+  const [destination, setDestination] = useState(' 41.26444007396637, -96.0901829202387')
+  // 41.26444007396637, -96.0901829202387
+  if (!isLoaded) {
+    return <SkeletonText />
+  }
+  async function calculateRoute() {
+  
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin,
+      destination,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    setOrigin('')
+    setDestination('')
+  }
+
+  return (
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
+      h='100vh'
+      w='100vw'
+    >
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={map => setMap(map)}
+        >
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </Box>
+      <Box
+        p={4}
+        borderRadius='lg'
+        m={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='1'
+      >
+        <HStack spacing={2} justifyContent='space-between'>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input
+                type='text'
+                placeholder='Origin'
+                value={origin}
+                onChange={(e) => setOrigin(e.target.value)}
+              />
+            </Autocomplete>
+          </Box>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input
+                type='text'
+                placeholder='Destination'
+                value={destination}
+                onChange={(e) => setDestination(e.target.value)}
+              />
+            </Autocomplete>
+          </Box>
+
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              icon={<FaTimes />}
+              onClick={clearRoute}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: {distance} </Text>
+          <Text>Duration: {duration} </Text>
+        </HStack>
+      </Box>
+    </Flex>
+  )
+}
+
+export default App
+
+
+// AIzaSyCIWbUh6hK1P_ARYXLVwqm2B_IOeACS8is
---
+++ frontend/src/Components/Track.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Maps from './Maps'
+function Track() {
+  return (
+    <div>
+        <Maps/>
+    </div>
+  )
+}
+
+export default Track
