b7c0595b15d4c9b9ac7d252d3151314d5584dbf8
JAGADEESH PONNAM <112121032+Jagadeeshponnam@users.noreply.github.com>
2023-11-08 14:38:50 -0600

    Update Reservation.jsx
    Reservation timing updated

--- frontend/src/Pages/Reservation.jsx
+++ frontend/src/Pages/Reservation.jsx
@@ -7,21 +7,21 @@ import { Select } from "@chakra-ui/react";
 import { get } from "../api/ApiService";
 import { useLocation } from 'react-router-dom';
 const hours = [
-  "9:00 AM",
-  "10:00 AM",
+  //"9:00 AM",
+  //"10:00 AM",
   "11:00 AM",
   "12:00 PM",
   "1:00 PM",
   "2:00 PM",
-  "3:00 PM",
+ // "3:00 PM",
   "4:00 PM",
   "5:00 PM",
   "6:00 PM",
   "7:00 PM",
   "8:00 PM",
   "9:00 PM",
-  "10:00 PM",
-  "11:00 PM",
+  //"10:00 PM",
+  //"11:00 PM",
 ];
 
 function Reservation() {
